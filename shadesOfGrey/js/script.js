d3761d63c2631123809daa3f145a8c1122c81811
Bourgois Aaron <tobourgoisaaron@gmail.com>
2020-01-30 15:49:07 +0100

    first commit

---
+++ shadesOfGrey/js/script.js
@@ -0,0 +1,29 @@
+$(window).scroll(function() {
+    var top_of_element = $("#facts").offset().top;
+    var bottom_of_element = $("#facts").offset().top + $("#facts").outerHeight();
+    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+    var top_of_screen = $(window).scrollTop();
+
+    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+        $('.counter').each(function() {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+
+            $({ countNum: $this.text()}).animate({
+                    countNum: countTo
+                },
+
+                {
+
+                    duration: 1000,
+                    easing:'linear',
+                    step: function() {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function() {
+                        $this.text(this.countNum);
+                    }
+                });
+        });
+    }
+});
